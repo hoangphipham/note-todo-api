9ea9dd65e7d63d8c70a7689dedd8a3184ee43c7d
MrPhiPhi\PhiPhi <phamnguyenhoangphi@gmail.com>
2017-12-20 21:05:34 +0700

    add invidual queries

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser')
-
+const {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose.js');
 var{Todo} = require('./models/todo');
 var{User} = require('./models/user');
@@ -28,6 +28,27 @@ app.get('/todos', (req, res)=>{
         res.status(400).send(e)
     })
 })
+
+
+//GET /todos/
+
+app.get('/todos/:id',(req,res)=>{
+    var id = req.params.id
+    //valid id
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
+    //findbyId
+    Todo.findById(id).then((todo)=>{
+        if(!todo){
+            return res.status(404).send();
+        }
+        res.send({todo});
+    }).catch((e)=>{
+        return res.status(404).send();
+    })
+})
+
 
 app.listen(3000 ,() => {
     console.log('on port 3000')
