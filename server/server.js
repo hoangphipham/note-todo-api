217654b5ff3a6d090c9152c826acdcb1cca9234c
MrPhiPhi\PhiPhi <phamnguyenhoangphi@gmail.com>
2017-12-21 09:14:41 +0700

    add server.js

--- server/server.js
+++ server/server.js
@@ -5,58 +5,105 @@ var {mongoose} = require('./db/mongoose.js');
 var{Todo} = require('./models/todo');
 var{User} = require('./models/user');
 
-var app = express();
+// var app = express();
 
+// const port = process.env.PORT || 3000;
+
+// app.use(bodyParser.json());
+
+// app.post('/todos', (req, res) => {
+//     var todo = new Todo({
+//       text: req.body.text
+//     });
+  
+//     todo.save().then((doc) => {
+//       res.send(doc);
+//     }, (e) => {
+//       res.status(400).send(e);
+//     });
+//   });
+
+// app.get('/todos', (req, res) => {
+//     Todo.find().then((todos) => {
+//       res.send({todos});
+//     }, (e) => {
+//       res.status(400).send(e);
+//     });
+//   });
+
+
+// //GET /todos/
+
+// app.get('/todos/:id',(req,res)=>{
+//     var id = req.params.id
+//     //valid id
+//     if(!ObjectID.isValid(id)){
+//         return res.status(404).send();
+//     }
+//     //findbyId
+//     Todo.findById(id).then((todo)=>{
+//         if(!todo){
+//             return res.status(404).send();
+//         }
+//         res.send({todo});
+//     }).catch((e)=>{
+//         return res.status(404).send();
+//     })
+// })
+
+
+// app.listen(port, () => {
+//     console.log(`Started up at port ${port}`);
+//   });
+  
+
+// module.exports = {app};
+
+var app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
-    var todo = new Todo({
-      text: req.body.text
-    });
-  
-    todo.save().then((doc) => {
-      res.send(doc);
-    }, (e) => {
-      res.status(400).send(e);
-    });
+  var todo = new Todo({
+    text: req.body.text
   });
+
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
 
 app.get('/todos', (req, res) => {
-    Todo.find().then((todos) => {
-      res.send({todos});
-    }, (e) => {
-      res.status(400).send(e);
-    });
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (e) => {
+    res.status(400).send(e);
   });
+});
 
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
 
-//GET /todos/
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
 
-app.get('/todos/:id',(req,res)=>{
-    var id = req.params.id
-    //valid id
-    if(!ObjectID.isValid(id)){
-        return res.status(404).send();
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
     }
-    //findbyId
-    Todo.findById(id).then((todo)=>{
-        if(!todo){
-            return res.status(404).send();
-        }
-        res.send({todo});
-    }).catch((e)=>{
-        return res.status(404).send();
-    })
-})
 
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
 
 app.listen(port, () => {
-    console.log(`Started up at port ${port}`);
-  });
-  
+  console.log(`Started up at port ${port}`);
+});
 
 module.exports = {app};
-
-//hello
