2f295e7741caad5366498623a32a384c41f37cf9
MrPhiPhi\PhiPhi <phamnguyenhoangphi@gmail.com>
2017-12-21 10:49:17 +0700

    add unit test delete

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -101,3 +101,42 @@ describe('GET /todos/:id' , () => {
     });
 });
 
+describe('DELETE /todos/:id', ()=>{
+    it('should remove a todo', (done)=>{
+        var hexId = todos[1]._id.toHexString();
+        request(app)
+            .delete(`/todos/${hexId}`)
+            .expect(200)
+            .expect((res)=>{
+                expect(res.body.todo._id).toBe(hexId);
+            })
+            .end((err,res)=>{
+                if(err){
+                    return done(err)
+            }
+
+            Todo.findById(hexId).then((todo)=>{
+                expect(todo).toNotExist();
+                done();
+                }).catch((e)=>{
+                    done(e);
+                })
+            })
+    });
+
+    it('should return 404 if a todo not found', (done)=>{
+        var hexID = new ObjectID().toHexString();
+        request(app)
+            .get(`/todos/${hexID}`)
+            .expect(404)
+            .end(done);
+    });
+
+    it('should return 404 if a object ID invalid', (done)=>{
+        request(app)
+            .delete('/todos/124abc')
+            .expect(404)
+            .end(done);
+    })
+})
+
