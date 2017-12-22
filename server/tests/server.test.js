c6f31748255f03c5ac4f148edcc1ff4febdb586b
MrPhiPhi\PhiPhi <phamnguyenhoangphi@gmail.com>
2017-12-22 09:45:59 +0700

    complete mongodb

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -22,28 +22,28 @@ beforeEach((done) =>{
 });
 
 describe('POST /todos', () => {
-    it('should create a new todo',(done) =>{
-        var text = 'test todo text';
-
+    it('should create a new todo', (done) => {
+        var text = 'Test todo text';
+    
         request(app)
-            .post('/todos')
-            .send({text})
-            .expect(200)
-            .expect((res)=>{
-                expect(res.body.text).toBe(text);
-            })
-            .end((err, res) =>{
-                if(err){
-                    return done(err);
-                }
-
-                Todo.find({text}).then((todos)=>{
-                    expect(todos.length).toBe(1);
-                    expect(todos[0].text).toBe(text);
-                    done();
-                }).catch((err) => done(err));
-            });
-    });
+          .post('/todos')
+          .send({text})
+          .expect(200)
+          .expect((res) => {
+            expect(res.body.text).toBe(text);
+          })
+          .end((err, res) => {
+            if (err) {
+              return done(err);
+            }
+    
+            Todo.find({text}).then((todos) => {
+              expect(todos.length).toBe(1);
+              expect(todos[0].text).toBe(text);
+              done();
+            }).catch((e) => done(e));
+          });
+      });
 
     it('should not create todo with invalid body data', (done) => {
         request(app)
