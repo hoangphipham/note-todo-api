c227d8b11036c92cb14e1f533aed1399810b8441
MrPhiPhi\PhiPhi <phamnguyenhoangphi@gmail.com>
2017-12-22 10:43:03 +0700

    test desploy 2

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,11 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
+
+var MONGODB_URI = 'mongodb://phipham:Hoang05phi12@ds157653.mlab.com:57653/node-todo-api'
+
 mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = { mongoose};
+
+
